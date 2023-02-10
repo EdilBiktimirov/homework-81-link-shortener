3a549e5292321e58ca1e83b580bd9fd15a0308a5
Edil Biktimirov <idyabik@gmail.com>
2023-02-10 13:08:56 +0600

    Added post, get requests to routes/url.ts

---
+++ api/routes/url.ts
@@ -0,0 +1,47 @@
+import express from "express";
+import {Url} from "../types";
+import ShortUrl from "../models/url";
+
+const urlRouter = express.Router();
+
+urlRouter.post('/links', async (req, res) => {
+  try {
+    const currentShortUrl = await ShortUrl.findOne({shortUrl: req.body.shortUrl});
+
+    if (!currentShortUrl) {
+      const newUrl: Url = {
+        shortUrl: req.body.shortUrl,
+        originalUrl: req.body.originalUrl,
+      };
+
+      const currentUrl = new ShortUrl(newUrl);
+      try {
+        await currentUrl.save();
+        return res.send(currentUrl);
+      } catch (error) {
+        return res.status(400).send(error);
+      }
+    } else {
+      return res.send({message: 'Incorrectly generated short url'})
+    }
+  } catch (e) {
+    return res.sendStatus(500);
+  }
+});
+
+urlRouter.get('/:shortUrl', async (req, res) => {
+  try {
+    const result = await ShortUrl.findOne({shortUrl: req.params.shortUrl});
+
+    if (!result) {
+      return res.sendStatus(404);
+    }
+    res.status(301).redirect(result.originalUrl);
+
+  } catch (e) {
+    return res.sendStatus(500);
+  }
+})
+
+
+export default urlRouter;
